ea7407c8e01f60722179f47c3c535b644f5fdeea
okor <jason.ormand1@gmail.com>
2014-03-19 23:03:54 -0400

    more compat with ruby 1.8

--- lib/schemapper.rb
+++ lib/schemapper.rb
@@ -8,16 +8,16 @@ nodes = tables.map do |table|
   { :name => table }
 end
 
-all_links = models.each_with_index.flat_map do |model, index|
+all_links = models.each_with_index.map do |model, index|
   associations = model.reflections.map do |reflection|
     target = reflection[1].table_name rescue nil
     if target && tables.index(target)
-      { :source => index, :target => tables.index(target), :value => rand(1..5) }
+      { :source => index, :target => tables.index(target), :value => rand(5) }
     end
   end
   associations.compact
 end
-links = all_links.select(&:present?)
+links = all_links.flatten.select(&:present?)
 
 attrs = models.map do |model|
   { :table => model.table_name, :attrs => model.attribute_names }
