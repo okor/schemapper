a4e1459d8588799ffb5dc5e1fc035b17d59b4c32
okor <jason.ormand1@gmail.com>
2014-03-19 22:31:18 -0400

    ughh

--- lib/schemapper.rb
+++ lib/schemapper.rb
@@ -5,14 +5,14 @@ models = ActiveRecord::Base.descendants
 tables = models.map(&:table_name)
 
 nodes = tables.map do |table|
-  { name: table }
+  { :name => table }
 end
 
 all_links = models.each_with_index.flat_map do |model, index|
   associations = model.reflections.map do |reflection|
     target = reflection[1].table_name rescue nil
     if target && tables.index(target)
-      { source: index, target: tables.index(target), value: rand(1..5) }
+      { :source => index, :target => tables.index(target), :value => rand(1..5) }
     end
   end
   associations.compact
@@ -20,7 +20,7 @@ end
 links = all_links.select(&:present?)
 
 attrs = models.map do |model|
-  { table: model.table_name, attrs: model.attribute_names }
+  { :table => model.table_name, :attrs => model.attribute_names }
 end
 
 puts YAML.dump({ :nodes => nodes, :links => links, :attributes => attrs.uniq })
